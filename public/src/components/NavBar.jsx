6023f3ab96dc8cc44bb28fc3309c2ac5ea0c9563
Tae Lee <talee@estee.com>
2018-03-28 18:06:16 -0400

    updated new files for navbar

---
+++ public/src/components/NavBar.jsx
@@ -0,0 +1,123 @@
+import _ from "lodash";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Icon,
+  Image,
+  Menu,
+  Sidebar,
+  Responsive
+} from "semantic-ui-react";
+
+const leftItems = [
+  { as: "a", content: "Home", key: "home" },
+  { as: "a", content: "Users", key: "users" }
+];
+const rightItems = [
+  { as: "a", content: "Login", key: "login" },
+  { as: "a", content: "Register", key: "register" }
+];
+
+const NavBarMobile = ({
+  children,
+  leftItems,
+  onPusherClick,
+  onToggle,
+  rightItems,
+  visible
+}) => (
+  <Sidebar.Pushable>
+    <Sidebar
+      as={Menu}
+      animation="overlay"
+      icon="labeled"
+      inverted
+      items={leftItems}
+      vertical
+      visible={visible}
+    />
+    <Sidebar.Pusher
+      dimmed={visible}
+      onClick={onPusherClick}
+      style={{ minHeight: "100vh" }}
+    >
+      <Menu fixed="top" inverted>
+        <Menu.Item>
+          <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
+        </Menu.Item>
+        <Menu.Item onClick={onToggle}>
+          <Icon name="sidebar" />
+        </Menu.Item>
+        <Menu.Menu position="right">
+          {_.map(rightItems, item => <Menu.Item {...item} />)}
+        </Menu.Menu>
+      </Menu>
+      {children}
+    </Sidebar.Pusher>
+  </Sidebar.Pushable>
+);
+
+const NavBarDesktop = ({ leftItems, rightItems }) => (
+  <Menu fixed="top" inverted>
+    <Menu.Item>
+      <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
+    </Menu.Item>
+    {_.map(leftItems, item => <Menu.Item {...item} />)}
+    <Menu.Menu position="right">
+      {_.map(rightItems, item => <Menu.Item {...item} />)}
+    </Menu.Menu>
+  </Menu>
+);
+
+const NavBarChildren = ({ children }) => (
+  <Container style={{ marginTop: "5em" }}>{children}</Container>
+);
+
+class NavBar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false
+    };   
+    this.handlePusher = this.handlePusher.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handlePusher() {
+    const { visible } = this.state;
+    if (visible) this.setState({ visible: false });
+  };
+
+  handleToggle() {
+    this.setState({ visible: !this.state.visible })
+  };
+
+  render() {
+    const { children, leftItems, rightItems } = this.props;
+    const { visible } = this.state;
+
+    return (
+      <div>
+        <Responsive {...Responsive.onlyMobile}>
+          <NavBarMobile
+            leftItems={leftItems}
+            onPusherClick={this.handlePusher}
+            onToggle={this.handleToggle}
+            rightItems={rightItems}
+            visible={visible}
+          >
+            <NavBarChildren>{children}</NavBarChildren>
+          </NavBarMobile>
+        </Responsive>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          <NavBarDesktop leftItems={leftItems} rightItems={rightItems} />
+          <NavBarChildren>{children}</NavBarChildren>
+        </Responsive>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
