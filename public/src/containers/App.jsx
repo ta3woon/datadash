a18dc04c4cd7aea97c26538e0091955e81f2498c
Tae Lee <talee@estee.com>
2018-03-28 18:05:46 -0400

    added component navbar cleaned up dependancies

--- public/src/containers/App.jsx
+++ public/src/containers/App.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import '../../assets/css/App.css';
+import NavBar from '../components/NavBar';
+import OldMain from './OldMain';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <NavBar>
+          <Image src="https://react.semantic-ui.com/assets/images/wireframe/paragraph.png" />
+        </NavBar>
         <header className="App-header">
-          <h1 className="App-title">DTIG Dashboard</h1>
+          <h1 className="App-title">DTIG AppJsx Dashboard</h1>
         </header>
+        <OldMain />
       </div>
     );
   }
